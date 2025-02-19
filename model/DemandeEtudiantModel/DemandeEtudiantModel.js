4ab5645e2cbd8bd59aec177ef3e4f388961f869e
amani-berrebeh <berrebehamani@gmail.com>
2025-02-19 10:30:05 +0100

    demande etudiant

--- model/DemandeEtudiantModel/DemandeEtudiantModel.js
+++ model/DemandeEtudiantModel/DemandeEtudiantModel.js
@@ -4,12 +4,13 @@ const Schema = mongoose.Schema;
 const DemandeEtudiantSchema = new Schema({
   studentId: { type: Schema.Types.ObjectId, ref: 'Etudiant', required: true },
   title: { type: String, required: false },
-  description: { type: String, required: true },
+  description: { type: String },
   piece_demande:{type: Schema.Types.ObjectId, ref: 'TemplateBody', required: false, default : null},
   langue:{type: String, required: false},
   nombre_copie:{type: Number, required: false},
   response: {type: String, required: false},
-  status: { type: String, enum: ['en attente', 'traité', 'rejeté'], default: 'en attente' },
+  status: { type: String, enum: ['en attente', 'traité', 'rejeté'], default: 'en attente' }, //TODO: Add new status : notifié 
+  generated_doc: {type: Schema.Types.ObjectId, ref: 'GeneratedDocModel', required: false, default : null},
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
 });
